6455d7f290701a67c211cfa49b732aa91a7e6209
Andrey Kozyrev <akozyrev@fitbit.com>
2016-08-08 13:22:39 +0300

    add nvm

---
+++ nvm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. defs.sh
+./init.sh
+
+mkdir ~/.nvm
+
+VER=v0.31.4
+
+pushd $USR/opt
+
+fetch_tar https://github.com/creationix/nvm/archive/v0.31.4.tar.gz nvm
+
+link_lib nvm
+
+update_env NVM_HOME '$USR/lib/nvm'
+update_env NVM_DIR '~/.nvm'
+update_src '$NVM_HOME/nvm.sh'
+
+popd
+
